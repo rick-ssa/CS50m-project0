158f07c983d770ce6f9d1037996c395e64b488e6
Ricardo <rick_ssa@hotmail.com>
2019-11-23 21:35:12 -0300

    adding the delete button and few css styles

--- script.js
+++ script.js
@@ -11,17 +11,36 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
   const text = prompt('Type the text of the task')
+  if (!text) return
+  
   const check = document.createElement('input')
   const li = document.createElement('li')
   const span = document.createElement('span')
+  const del = document.createElement('button')
+  const id = 'id' + count()
+  
+  li.id = 'item' + id
+  check.id = 'check' + id
+  span.id = 'span' + id
 
+  del.innerHTML = '-'
+  
   check.type = 'checkbox'
   check.classList.add('todo-checkbox')
-  check.addEventListener('change',function(){changeUnchekedCountSpan(this)})
-  span.appendChild(document.createTextNode(text))
+
+  check.addEventListener('change',function(){changeUnchekedCountSpan(this,document.getElementById('span' + id))})
+  del.addEventListener('click',()=>{
+    decCount()
+    if (!document.getElementById('check' + id).checked)  decUnchecedCount()
+    list.removeChild(document.getElementById('item' + id))
+  })
+
+  span.appendChild(document.createTextNode(text))  
   li.appendChild(check)
   li.appendChild(span)
+  li.appendChild(del)
   list.appendChild(li)
+
   incCount()
   incUnchecedCount()
 }
@@ -42,10 +61,20 @@ function decUnchecedCount() {
   uncheckedCountSpan.innerHTML = Number(uncheckedCountSpan.innerHTML) - 1
 }
 
-function changeUnchekedCountSpan(ele) {
-  if (ele.checked === true) {
+function changeUnchekedCountSpan(chk, lbl) {
+  if (chk.checked === true) {
+    lbl.style.textDecoration = 'line-through'
     decUnchecedCount()
   } else {
+    lbl.style.textDecoration = 'none'
     incUnchecedCount()
   }
 }
+
+const count = (function() {
+  var count = 0;
+
+  return ()=>{
+    return count += 1;
+  }
+})()
